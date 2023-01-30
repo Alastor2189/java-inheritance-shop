a97fa03e1a0b5306729429f7ed4440ac30b20e85
Alastor2189 <g.lucafarci89@gmail.com>
2023-01-30 17:17:52 +0100

    aggiunta cuffie

--- java/lessons/org/prodotto2/Cuffie.java
+++ java/lessons/org/prodotto2/Cuffie.java
@@ -1,5 +1,32 @@
 package java.lessons.org.prodotto2;
 
-public class Cuffie {
-    
+public class Cuffie extends Prodotto {
+	
+	private String coloreCuffie;
+	private Boolean cuffieWireless;
+	public Cuffie(String nome, String marca, double prezzo, String coloreCuffie, Boolean cuffieWireless) {
+		super(nome, marca, prezzo);
+		this.coloreCuffie = coloreCuffie;
+		this.cuffieWireless = cuffieWireless;
+	}
+	
+	public String getColore() {
+		return coloreCuffie;
+	}
+	
+	public void setColore(String coloreCuffie) {
+		this.coloreCuffie = coloreCuffie;
+	}
+	
+	public boolean getCuffieWireless() {
+		return cuffieWireless;
+	}
+	
+	public void setCuffieWireless(Boolean cuffiewireless) {
+		this.cuffieWireless = cuffieWireless;
+	}
+	
+	
+	
 }
+
