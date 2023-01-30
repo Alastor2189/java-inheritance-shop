8f4b82b058cbbb2cd278389e9d637a198c21e33e
Alastor2189 <g.lucafarci89@gmail.com>
2023-01-30 17:03:43 +0100

    completamento classe prodotto

--- java/lessons/org/prodotto2/Main.java
+++ java/lessons/org/prodotto2/Main.java
@@ -1,25 +1,25 @@
-package java.lessons.org.prodotto2;
+// package java.lessons.org.prodotto2;
 
-import java.util.Scanner;
-public class Main {
+// import java.util.Scanner;
+// public class Main {
 	
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
+// 	public static void main(String[] args) {
+// 		Scanner s = new Scanner(System.in);
 		
-		System.out.println("Nome Prodotto:");
-		String nome = s.nextLine().trim().toLowerCase();
+// 		System.out.println("Nome Prodotto:");
+// 		String nome = s.nextLine().trim().toLowerCase();
 		
-		System.out.println("Descrizione del prodotto:");
-		String descrizione = s.nextLine().trim();
+// 		System.out.println("Descrizione del prodotto:");
+// 		String descrizione = s.nextLine().trim();
 		
-		System.out.println("Prezzo imponibile:");
-		float prezzo = Float.parseFloat(s.nextLine());
+// 		System.out.println("Prezzo imponibile:");
+// 		float prezzo = Float.parseFloat(s.nextLine());
 		
-		Prodotto prodotto = new Prodotto(nome, descrizione, prezzo);
+// 		Prodotto prodotto = new Prodotto(nome, descrizione, prezzo);
 		
-		System.out.println("Nome Prodotto Completo: " + prodotto.nomeCompletoProdotto());
-		System.out.println("Descrizione del prodotto: " + prodotto.descrizioneProdotto());
-		System.out.println("Prezzo Base (iva escl.): " + prodotto.prezzoImponibile());
-		System.out.println("Prezzo con Iva: " + prodotto.prezzoFinale());
-	}
-}
+// 		System.out.println("Nome Prodotto Completo: " + prodotto.nomeCompletoProdotto());
+// 		System.out.println("Descrizione del prodotto: " + prodotto.descrizioneProdotto());
+// 		System.out.println("Prezzo Base (iva escl.): " + prodotto.prezzoImponibile());
+// 		System.out.println("Prezzo con Iva: " + prodotto.prezzoFinale());
+// 	}
+// }
--- java/lessons/org/prodotto2/Prodotto.java
+++ java/lessons/org/prodotto2/Prodotto.java
@@ -3,38 +3,56 @@ package java.lessons.org.prodotto2;
 import java.util.Random;
 
 public class Prodotto {
-	int codice;
-	String nome;
-	String descrizione;
-	float prezzo;
-	float iva = 22;
+	private int codice;
+	private String nome;
+	private String marca	;
+	private double prezzo;
+	private float iva = 22;
 	
-	public Prodotto(String nome, String descrizione, float prezzo) {
+	public Prodotto(String nome, String marca, double prezzo) {
 		this.nome = nome;
-		this.descrizione = descrizione;
+		this.marca = marca;
 		this.prezzo = prezzo;
 		
 		Random rand = new Random();
 		this.codice = rand.nextInt(100000000);
 	}
+	public String getCodiceIdentificativo() {
+		return String.format("%08d", this.codice);
+	}
 	
-	public float prezzoImponibile() {
+	public String getNome() {
+		return nome;
+	}
+	
+	public String getMarca() {
+		return this.marca;
+	}
+	
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	
+	public void setNome(String nome) {
+		 this.nome = nome;
+	}
+	
+	public double getPrezzo() {
 		return this.prezzo;
 	}
 	
-	public float prezzoFinale() {
+	public void setPrezzo(double prezzo) {
+		this.prezzo = prezzo;
+	}
+	
+	public float getIva() {
+		return this.iva;
+	}
+	
+	public void setIva(float iva) {
+		this.iva = iva;
+	}
+	
+	public double prezzoFinale() {
 		return this.prezzo * ( this.iva/100 + this.prezzo );
 	}
-	
-	public String nomeCompletoProdotto() {
-		return this.codiceIdentificativo() + "-" + this.nome;
-	}
-	
-	public String descrizioneProdotto() {
-		return this.descrizione;
-	}
-	
-	public String codiceIdentificativo() {
-		return String.format("%08d", this.codice);
-	}
-}
