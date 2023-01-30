745ae6d947adff6e047c234fe46e67f3a5e6407e
Alastor2189 <g.lucafarci89@gmail.com>
2023-01-30 14:54:38 +0100

    primo commit, riprendo l'esercizio precedente e riparto

---
+++ java/lessons/org/prodotto2/Prodotto.java
@@ -0,0 +1,40 @@
+package java.lessons.org.prodotto2;
+
+import java.util.Random;
+
+public class Prodotto {
+	int codice;
+	String nome;
+	String descrizione;
+	float prezzo;
+	float iva = 22;
+	
+	public Prodotto(String nome, String descrizione, float prezzo) {
+		this.nome = nome;
+		this.descrizione = descrizione;
+		this.prezzo = prezzo;
+		
+		Random rand = new Random();
+		this.codice = rand.nextInt(100000000);
+	}
+	
+	public float prezzoImponibile() {
+		return this.prezzo;
+	}
+	
+	public float prezzoFinale() {
+		return this.prezzo * ( this.iva/100 + this.prezzo );
+	}
+	
+	public String nomeCompletoProdotto() {
+		return this.codiceIdentificativo() + "-" + this.nome;
+	}
+	
+	public String descrizioneProdotto() {
+		return this.descrizione;
+	}
+	
+	public String codiceIdentificativo() {
+		return String.format("%08d", this.codice);
+	}
+}
