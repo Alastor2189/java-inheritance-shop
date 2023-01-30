9919a6c93f009b766a4e8104563803485ccee57b
Alastor2189 <g.lucafarci89@gmail.com>
2023-01-30 17:42:10 +0100

    inserimento televisore

--- java/lessons/org/prodotto2/Televisori.java
+++ java/lessons/org/prodotto2/Televisori.java
@@ -1,5 +1,32 @@
 package java.lessons.org.prodotto2;
 
-public class Televisori {
-    
+public class Televisore extends Prodotto {
+	
+	private String tvSize;
+	private Boolean smartTv;
+	
+	
+	public Televisore(String nome, String marca, double prezzo, String tvSize, Boolean smartTv) {
+		super(nome, marca, prezzo);
+		this.tvSize = tvSize;
+		this.smartTv = smartTv;
+	}
+
+	public String getTvSize() {
+		return tvSize;
+	}
+	
+	public void setTvSize(String tvSize) {
+		this.tvSize = tvSize;
+	}
+	
+	public Boolean isSmartTv() {
+		return smartTv;
+	}
+	
+	public void setSmartTv(Boolean smartTv) {
+		this.smartTv = smartTv;
+	}
+	
+	
 }
